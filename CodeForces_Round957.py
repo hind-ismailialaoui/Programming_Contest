ac857a1db27e1d97612db5d10e79fc1dc719f727
Hind ISMAILI ALAOUI <83179105+hind-ismailialaoui@users.noreply.github.com>
2024-07-12 08:51:54 +0200

    constest

--- CodeForces_Round957.py
+++ CodeForces_Round957.py
@@ -1,3 +1,5 @@
+# A. Only Pluses
+
 t = (int(input()))
 # s = []
 for _ in range(t):
@@ -5,7 +7,14 @@ for _ in range(t):
     l = [a,b,c]
 
     val_max = max(a,b,c)
+    val_min= min(a,b,c)
+    val_milieu= between_min_max = [x for x in l if x != val_min and x != val_max][0]
     nb_occ= l.count(val_max)
 
     print(val_max)
     print(nb_occ)
+
+    if nb_occ == 1:
+        for i in range(5):
+            val_min = val_min + i
+            
