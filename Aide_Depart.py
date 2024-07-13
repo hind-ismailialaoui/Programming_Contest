0846a8fa9b03c5906c87564acbd1575514a23ffd
Hind ISMAILI ALAOUI <83179105+hind-ismailialaoui@users.noreply.github.com>
2024-07-13 12:14:21 +0200

    mise en forme

---
+++ Aide_Depart.py
@@ -0,0 +1,19 @@
+######################## Ecritures de départ ##############################
+
+# Pour les tests
+t = int(input())
+for _ in range(t):
+    break
+
+# Les uns en dessous des autres
+N = int(input())
+S = [input() for _ in range(N)]
+
+#Les uns à coté des autres (1 variables)
+n = int(input())
+a = list(map(int, input().split()))
+
+# Les uns à coté des autres (2 variables)
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
