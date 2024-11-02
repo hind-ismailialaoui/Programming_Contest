6e5d3063bf8e52d07149b8edbb1b636306ba4918
Hind ISMAILI ALAOUI <ismailialhin@gmail.com>
2024-11-02 15:19:27 +0100

    concours de lq semaine derniere

---
+++ .vscode/Untitled-1.js
@@ -0,0 +1,10 @@
+let print = console.log
+goo = "123";
+foo = "1";
+
+good= parseInt(goo)
+food=parseInt(foo)
+
+print(good-food)
+
+
---
+++ AtCoder_BeginnerContest377.py
@@ -0,0 +1,9 @@
+# A
+
+S = str(input())
+
+if S.count('A') == 1 and S.count('B') == 1 and S.count('C')==1:
+    print('Yes')
+else: 
+    print('No')
+
